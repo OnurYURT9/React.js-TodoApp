c7ad1221f2cb34d6399728c81501621b9a06a05f
ONUR YURT <57957917+OnurYURT9@users.noreply.github.com>
2021-07-29 17:23:31 +0300

    Add files via upload

--- React.js Dersleri/HELLO_REACT/src/classes.js
+++ React.js Dersleri/HELLO_REACT/src/classes.js
@@ -1,27 +1,70 @@
-// var Person = function () {
-
-// }
-
-class Person { 
-    constructor(name="Guess",age=10) {
-        this.name  = name;
-        this.age = age;
-        console.log('constructer çalıştı');
+class Person {
+    constructor(name = 'Guest', year = 1900) {
+        this.name = name;
+        this.year = year;
+        console.log('constructor çalıştı.');
     }
-    calculateAge(){
-        return new Date().getFullYear() - this.age;
+
+    calculateAge() {
+        return new Date().getFullYear() - this.year;
     }
-    greeting(text){
-        return `${text},my name is ${this.name}`;
+
+    greeting(text) {
+        return `${text}, My name is ${this.name}. `;
     }
 }
-const p1 = new Person("Onur",22);
-const p2 = new Person("Emine",28);
+
+class Student extends Person {
+    constructor(name,year,studentNumber) {
+        super(name,year);
+        this.studentNumber = studentNumber;
+    }
+
+    study() {
+        console.log('I am learning');
+    }
+
+    greeting(text) {
+        let str = super.greeting(text);
+        str += `My number is ${this.studentNumber}`;
+        return str;
+    }
+}
+
+class Teacher extends Person {
+    constructor(name,year,department) {
+        super(name,year,department);
+        this.department = department;
+    }
+    teach() {
+        console.log('I am teaching math');
+    }
+
+    greeting(text) {
+        let str = super.greeting(text);
+        str += `My department is ${this.department}`;
+        return str;
+    }
+}
+
+// Nesne - Object
+
+const p1 = new Student("Onur", 1999, 120);
+const p2 = new Teacher("Emine", 1993, 'Math');
 const p3 = new Person();
 
+p1.study();
+p2.teach();
+
+
+
 console.log(p1.greeting('Hello'));
+console.log(p2.greeting('Good morning'));
+console.log(p3.greeting('Hello'));
+
 console.log(p1.calculateAge());
 console.log(p2.calculateAge());
-console.log(p2);
+console.log(p3.calculateAge());
+
 console.log(p1);
-console.log(p3);
+console.log(p2);
